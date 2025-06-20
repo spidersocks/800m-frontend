16c3b186e40a9975d3e8d20f953e845260bb2cf2
spidersocks <sfontaine20@ucla.edu>
2025-06-21 06:54:10 +0800

    Testing changes to vite config file

--- tailwindcss4/vite.config.js
+++ tailwindcss4/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/800m-calculator/', 
+  // base: '/800m-calculator/', 
   plugins: [react(),
             tailwindcss()],
 })
